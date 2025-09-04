2b66d7cba89acc47a6a59cc508e8a16797461931
Juan Andres Nuñez <nunezlopez.ja@gmail.com>
2025-09-03 18:12:10 -0600

    Wrote a little logic. Will continue tomorrow

--- src/main.c
+++ src/main.c
@@ -1,14 +1,41 @@
 #include <raylib.h>
-#include <stdio.h>
+
+#define SCREEN_WIDTH 1280
+#define SCREEN_HEIGHT 720
+
+#define PLAYER_WIDTH 150.0f
+#define PLAYER_HEIGHT 20.0f
+
+typedef struct {
+    Rectangle rect;
+    Color color;
+} Player;
+
+void updatePlayer(Player *player, float delta) {
+    float max_x = SCREEN_WIDTH - PLAYER_WIDTH;
+}
 
 int main(void) {
-    printf("Here I go killing again!\n");
+    InitWindow(SCREEN_WIDTH, SCREEN_HEIGHT, "Breakout1");
+    float deltaTime = GetFrameTime();
 
-    InitWindow(1280, 720, "Breakout1");
+    // Initializing player
+    Player player = {0};
+    player.rect = (Rectangle){(SCREEN_WIDTH / 2.0f) - 75, SCREEN_HEIGHT - 80,
+                              PLAYER_WIDTH, PLAYER_HEIGHT};
+    player.color = BROWN;
 
     while (!WindowShouldClose()) {
+        // Update
+        // -------------------------
+        updatePlayer(&player);
+
         BeginDrawing();
         ClearBackground(SKYBLUE);
+
+        // Draw elements
+        DrawRectangleRec(player.rect, player.color);
+
         EndDrawing();
     }
 
